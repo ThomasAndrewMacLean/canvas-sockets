c5e0245fdd56573a2d56f3567b7c44502734854e
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-03-01 07:10:41 +0100

    setup TS project 🎉🚀

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # canvas-sockets
 play around with socket IO
+
+https://socket.io/get-started/chat/
+
+https://github.com/paullewis/pocket-money
+
+https://medium.com/javascript-in-plain-english/typescript-with-node-and-express-js-why-when-and-how-eb6bc73edd5d
---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express = require('express');
+import socket = require('socket.io');
+const app = express();
+const http = require('http').Server(app);
+const io = socket(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+    console.log('a user connected', socket.id);
+
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected', socket.id);
+    });
+});
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
