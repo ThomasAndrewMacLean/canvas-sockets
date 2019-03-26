e79b127e5443edf6fe075d96d66735f261b9890b
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-03-26 20:20:14 +0100

    test

--- src/app.ts
+++ src/app.ts
@@ -58,7 +58,7 @@ const saveItem = (imageUrl: string, slug: string) => {
 app.use(express.static('src/public'));
 
 app.post('/testing', (req, res) => {
-    return res.status(200).json(process.env.ACCESS_KEY_ID);
+    return res.status(200).json(process.env.SECRET_ACCESS_KEY);
 });
 
 app.get('/uuid', (req, res) => {
